769c961964be19d0901c7d348c11a9040f8cc4ec
Toni Bartylla <bartyllatoni@gmail.com>
2021-04-04 11:17:43 +0200

    completed tasks

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+;
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var years = document.getElementById("years").value
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("interest").value;
+    var interest = principal * years * rate / 100;
+    var result = document.getElementById("result");
+    var futureYear = new Date().getFullYear() + parseInt(years);
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else{
+        result.innerHTML = "</br>If you deposit <b>" + principal + "</b></br>" + 
+            "at an interest rate of <b>" + rate + "%</b>. <br>" + 
+            "You will receive an amount of <b>"+ interest + 
+            "</b>,</br>in the year <b>" + futureYear + "</b></br></br>"
+    }
 }
-        
+
+function displaySliderValue(){
+    var interest = document.getElementById("interest").value;
+    document.getElementById("slidervalue").textContent=interest;
+}
+
